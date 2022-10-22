35ae8526f21b11a3594295cfb19b079d2d91fee2
user <oksi.1213@mail.ru>
2022-10-22 16:08:12 +1000

    D004

---
+++ D004/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные чмсла от 1 до N
+ 5 -> 2, 4
+ 8 -> 2, 4, 6, 8
+*/
+int n;
+int i = 1;
+            bool not = true;
+
+            Console.WriteLine("Введите число:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чётные числа от 1 до " + n);
+            while (i <= n)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.Write(i + ", ");
+                    not = false;
+                }
+                i++;
+            }
+
+           
